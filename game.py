1e54f6f3d35834dcfcdf54e711c44321997daaea
Nick <Nick.Lucca.1018@gmail.com>
2013-10-02 10:53:06 -0400

    Update game.py
    -Added lines of code so that the ball can move up and down
    -Added a title to the window that opens when the code runs

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ speed = [0, 0]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption('The Game')
 ball = pygame.image.load("ball.gif")
 ballrect = ball.get_rect()
 
@@ -22,6 +23,12 @@ while 1:
                 speed[0] = -1
             else:
                 speed[0] = 0
+            if pygame.key.get_pressed()[K_w] and not pygame.key.get_pressed()[K_s]:
+                speed[1] = -1
+            elif pygame.key.get_pressed()[K_s] and not pygame.key.get_pressed()[K_w]:
+                speed[1] = 1
+            else:
+                speed[1] = 0
     ballrect = ballrect.move(speed)
     screen.fill(black)
     screen.blit(ball, ballrect)
